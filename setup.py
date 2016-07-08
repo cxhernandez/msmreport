88a645e9f3998a259e9963fc60af53906788b86d
Carlos Hernandez <cxh@stanford.edu>
2016-07-08 19:47:20 -0400

    fix cli

--- setup.py
+++ setup.py
@@ -2,23 +2,31 @@ from setuptools import setup, find_packages
 
 from msmreport import __version__, __name__
 
-setup(
-    name=__name__,
-    version=__version__,
-    packages=find_packages(),
-    include_package_data=True,
-    package_data={
-        'msmreport': ['assets/*.*', 'assets/templates/*.*']
-    },
-    zip_safe=True,
-    platforms=["Linux", "Unix"],
-    author="cxhernandez",
-    author_email="cxh@stanford.edu",
-    description="Create reports for MSMs generated using Osprey",
-    license="MIT",
-    entry_points={
-        'console_scripts': [
-            'msmreport = msmreport.cli.main:main',
-        ],
-    },
-)
+
+def main(**kwargs):
+    setup(
+        name=__name__,
+        version=__version__,
+        packages=find_packages(),
+        include_package_data=True,
+        package_data={
+            'msmreport': ['assets/*.*', 'assets/templates/*.*']
+        },
+        zip_safe=True,
+        platforms=["Linux", "Unix"],
+        author="cxhernandez",
+        author_email="cxh@stanford.edu",
+        description="Create reports for MSMs generated using Osprey",
+        license="MIT",
+        entry_points={
+            'console_scripts': [
+                'msmreport = msmreport.cli.main:main',
+            ],
+        },
+        **kwargs
+    )
+
+
+if __name__ == '__main__':
+    kwargs = {}
+    main(**kwargs)
