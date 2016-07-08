1b45a6e49a014681bc6e982efd937334b2b1e8fa
Carlos Hernandez <cxh@stanford.edu>
2016-07-08 19:15:06 -0400

    mk cli

---
+++ msmreport/cli/parser_create.py
@@ -0,0 +1,37 @@
+from __future__ import print_function, absolute_import, division
+from argparse import ArgumentDefaultsHelpFormatter
+
+from nbconvert.exporters.export import exporter_map
+
+
+def func(args, parser):
+    # delay import of the rest of the module to improve `osprey -h` performance
+    from os.path import splitext, basename
+    from .. import execute_nb, convert_nb
+    from ..utils import Timing, nb_template
+
+    yaml, tmp, fmt = parser.yaml, parser.tmp, parser.fmt
+
+    output = basename(splitext(yaml)[0])
+    with Timing('reading osprey config...'):
+        nb = nb_template(yaml, tmp=tmp)
+    with Timing('generating report...'):
+        nb = execute_nb(nb)
+    with Timing('saving as %s...' % fmt):
+        convert_nb(nb, output, fmt)
+
+
+def configure_parser(sub_parsers):
+    help = 'Generate a report for an MSM made with osprey'
+    p = sub_parsers.add_parser('plot', description=help, help=help,
+                               formatter_class=ArgumentDefaultsHelpFormatter)
+    p.add_argument('-y', '--yaml', dest='yaml',
+                   help='Path to Osprey config file')
+    p.add_argument('-t', '--template', dest='tmp',
+                   default=None,
+                   help='Path to ipynb')
+    p.add_argument('-f', '--format', dest='fmt',
+                   choices=exporter_map.keys(),
+                   default='html',
+                   help='Path to Osprey config file')
+    p.set_defaults(func=func)
--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 from setuptools import setup, find_packages
 
-from glob import glob
+from msmreport import __version__, __name__
 
 setup(
-    name="msmreport",
-    version="0.1",
+    name=__name__,
+    version=__version__,
     packages=find_packages(),
     include_package_data=True,
     package_data={
         'msmreport': ['assets/*.*', 'assets/templates/*.*']
     },
-    scripts=glob('./scripts/*.py'),
     zip_safe=True,
     platforms=["Linux", "Unix"],
     author="cxhernandez",
     author_email="cxh@stanford.edu",
     description="Create reports for MSMs generated using Osprey",
     license="MIT",
+    entry_points={
+        'console_scripts': [
+            'msmreport = msmreport.cli.main:main',
+        ],
+    },
 )
